4215adf215fa047354d2d230fc9f621d75e66fb2
Klfnovice <165791121+Klfnovice@users.noreply.github.com>
2024-06-05 15:19:11 +0800

    Update tabulation.py
    login

--- tabulation.py
+++ tabulation.py
@@ -82,7 +82,7 @@ if st.session_state.username is None:
     login_form.write("### Login")
     username = login_form.text_input("Username")
     password = login_form.text_input("Password", type="password")
-    submit_button = login_form.form_submit_button("Submit")
+    submit_button = login_form.form_submit_button("Login")
     # Directly authenticate the user
     if submit_button:
         if authenticate(username, password):
