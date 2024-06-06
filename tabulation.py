84435d8bafe7e3c8fa7e6de428bc50b8f54ec782
KLF_project <165791121+Klfnovice@users.noreply.github.com>
2024-06-06 14:26:08 +0800

    Update tabulation.py

--- tabulation.py
+++ tabulation.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import os
 
 # URL of the logo image in your GitHub repository
-logo_url = "https://raw.githubusercontent.com/Klfnovice/lnatabulation/main/lcwd%20logo.png?token=GHSAT0AAAAAACTIRZDVURWSUE6CQJYORFSMZTBJCKQ"
+logo_url = "https://raw.githubusercontent.com/Klfnovice/lnatabulation/main/lcwd%20logo.png?token=GHSAT0AAAAAACTIRZDULJ3TIYWSOBVLECVYZTBK2KA"
 
 # Add the logo and title with a border using HTML and CSS
 st.markdown(f"""
