16a9c5b9c30a226d1d5ce42afac3cfbbc63bb718
KLF_project <165791121+Klfnovice@users.noreply.github.com>
2024-07-04 14:39:53 +0800

    Update tabulation.py

--- tabulation.py
+++ tabulation.py
@@ -112,7 +112,7 @@ else:
 
                 # Example of using matplotlib for customized plots
                 if st.button("Show Chart"):
-                    fig, ax = plt.subplots(figsize=(20, 10))  # Adjust the figure size
+                    fig, ax = plt.subplots(figsize=(30, 10))  # Adjust the figure size
                     selected_data.plot(kind='bar', ax=ax)
                     title = competency_type.replace("_", " ").title() + " Identified"  # Dynamically set the title
                     ax.set_title(title, pad=20, fontsize=16)  # Add title above the chart with specified font size
@@ -165,7 +165,7 @@ else:
 
                 # Example of using matplotlib for customized plots
                 if st.button("Show Chart"):
-                    fig, ax = plt.subplots(figsize=(20, 10))  # Adjust the figure size
+                    fig, ax = plt.subplots(figsize=(30, 10))  # Adjust the figure size
                     selected_data.plot(kind='bar', ax=ax)
                     title = st.session_state.page.replace("_", " ").title() + " Identified"  # Dynamically set the title
                     ax.set_title(title, pad=20, fontsize=16)  # Add title above the chart with specified font size
