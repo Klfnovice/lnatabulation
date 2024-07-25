4a7694380c8d47a05ed6984715681498a0f39f45
KLF_project <165791121+Klfnovice@users.noreply.github.com>
2024-07-25 14:24:11 +0800

    Update tabulation.py

--- tabulation.py
+++ tabulation.py
@@ -447,7 +447,7 @@ def delete_data(user):
     conn.commit()
 
 # Login functionality
-st.sidebar.image("https://raw.githubusercontent.com/Klfnovice/lnatabulation/main/LCWD%20Logo%20-%20Copy.png, use_column_width=True)
+st.sidebar.image("https://raw.githubusercontent.com/Klfnovice/lnatabulation/main/LCWD%20Logo%20-%20Copy.png", use_column_width=True)
 st.sidebar.title('Login')
 
 if 'logged_in' not in st.session_state:
